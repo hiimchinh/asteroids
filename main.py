3bbb80cd08e4acf17e4598c4f3be24320feaf1a2
hiimchinh <chinhhm@proton.me>
2024-12-28 23:07:50 +0700

    rotate player with keypress

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    # delta time
     dt = 0
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill(color="Black")
         player.draw(screen)
+
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
