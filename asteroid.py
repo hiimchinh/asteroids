a91f8af14e42caf8a175dff450b3541f393353ca
hiimchinh <chinhhm@proton.me>
2025-01-10 23:06:19 +0700

    kill asteroid and shot when collide

--- asteroid.py
+++ asteroid.py
@@ -7,7 +7,7 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, 'red', self.position, radius=self.radius, width=2)
+        pygame.draw.circle(screen, 'white', self.position, radius=self.radius, width=2)
 
     def update(self, dt):
         self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -48,6 +48,10 @@ def main():
             if asteroid.check_collision(player):
                 print('Game over!')
                 sys.exit()
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.kill()
+                    shot.kill()
         screen.fill(color="Black")
         for i in drawable:
             i.draw(screen)
